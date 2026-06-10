3d3d16b5c05b7540f263123f2ff8f719ecbf10af
Flameh <184697136+FlamesW@users.noreply.github.com>
2026-06-10 17:38:34 +0300

    Update LoadingCaches.lua

--- Templates/LoadingCaches.lua
+++ Templates/LoadingCaches.lua
@@ -1,5 +1,5 @@
 -- // @Loading caches
-local Game, os_clock, loadstring, tostring, task_spawn = game, os.clock, loadstring, tostring, task.spawn
+local Game, os_clock, loadstring, tostring, task_spawn, warn = game, os.clock, loadstring, tostring, task.spawn, warn
 local write_file, read_file, is_file, make_folder, is_folder = writefile, readfile, isfile, makefolder, isfolder
 
 local Assets = {
