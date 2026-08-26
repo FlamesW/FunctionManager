7e4dea9510235f106f6e339b48ab190fa6134441
Flameh <184697136+FlamesW@users.noreply.github.com>
2026-08-26 11:29:46 +0300

    Update LoopManager.lua

--- Templates/LoopManager.lua
+++ Templates/LoopManager.lua
@@ -1,1 +1,36 @@
+if typeof(_IsLoadedFM) == "function" and _IsLoadedFM() then
+    warn("@Function Manager.luau is already running")
+    return
+end
 
+local Class = loadstring(game:HttpGet("https://github.com/FlamesW/FunctionManager/releases/latest/download/Module.luau"))()
+
+local API = Class.Launch({
+    Loop_Example = true
+})
+
+API:WhileLoop(0.35, function(self)
+    if self.Loop_Example then
+        print("wowwee")
+    end
+end, "Loop")
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/deividcomsono/Obsidian/main/Library.lua"))()
+
+local Window = Library:CreateWindow()
+local Main = Window:AddTab("Main", "user")
+
+local GroupBox = Main:AddLeftGroupbox("Groupbox", "boxes")
+
+GroupBox:AddToggle("Loop_Example_CFG", {
+    Text = "Loop Toggle",
+    Default = API.Loop_Example,
+    Callback = function(Value)
+        API.Loop_Example = Value
+    end
+})
+
+GroupBox:AddButton("Unload", function()
+    Library:Unload()
+    API:Unload()
+end)
